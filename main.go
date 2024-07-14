f88efaf07882179b2f4e5bbd7c70849069fde834
thehxdev <hossein.khosravi.ce@gmail.com>
2024-07-15 01:03:52 +0330

    handle SIGTERM and SIGINT signals
    Signed-off-by: thehxdev <hossein.khosravi.ce@gmail.com>

--- main.go
+++ main.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"flag"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
 	"github.com/thehxdev/txtban/txtban"
 )
 
@@ -12,5 +17,20 @@ func main() {
 	tb := txtban.Init(*configPath)
 	defer tb.Conn.DB.Close()
 
-	tb.ErrLogger.Fatal(tb.Run())
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGTERM, syscall.SIGINT)
+
+	go func() {
+        err := tb.Run()
+        if err != nil {
+            tb.ErrLogger.Println(err)
+        }
+	}()
+
+	_ = <-sigChan
+    tb.InfLogger.Println("Shutting down the server...")
+    err := tb.App.ShutdownWithTimeout(time.Second * 10)
+    if err != nil {
+        tb.ErrLogger.Println(err)
+    }
 }
