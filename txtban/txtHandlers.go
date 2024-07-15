812cc3b8b2e85f16db087bc035dad711d605a332
thehxdev <hossein.khosravi.ce@gmail.com>
2024-07-15 13:12:15 +0330

    removed cat handler comment
    Signed-off-by: thehxdev <hossein.khosravi.ce@gmail.com>

--- txtban/txtHandlers.go
+++ txtban/txtHandlers.go
@@ -6,33 +6,6 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-// func (t *Txtban) catHandler(c *fiber.Ctx) error {
-// 	authKey, err := getAuthKey(c.GetReqHeaders())
-// 	if err != nil {
-// 		return err
-// 	}
-// 	var jdata map[string]string
-//
-// 	if err := c.BodyParser(&jdata); err != nil {
-// 		t.ErrLogger.Println(err.Error())
-// 		return err
-// 	}
-//
-// 	user, err := t.Conn.AuthenticateByAuthKey(authKey)
-// 	if err != nil {
-// 		t.ErrLogger.Println(err.Error())
-// 		return err
-// 	}
-//
-// 	txt, err := t.Conn.GetTxtByName(user.ID, jdata["name"])
-// 	if err != nil {
-// 		t.ErrLogger.Println(err.Error())
-// 		return err
-// 	}
-//
-// 	return c.SendString(txt.Content)
-// }
-
 func (t *Txtban) readHandler(c *fiber.Ctx) error {
 	txtid := c.Params("txtid")
 	content, err := t.Conn.GetTxtContentById(txtid)
