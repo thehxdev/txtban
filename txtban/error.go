325b938afbbfaf8241c3ea7eadd3e4e946579774
thehxdev <hossein.khosravi.ce@gmail.com>
2024-07-17 01:54:05 +0330

    general error handling for handlers
    Signed-off-by: thehxdev <hossein.khosravi.ce@gmail.com>

---
+++ txtban/error.go
@@ -0,0 +1,19 @@
+package txtban
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/thehxdev/txtban/tberr"
+)
+
+var (
+	errEmptyAuthorizationHeader = tberr.New(fiber.ErrBadRequest.Error(), "set 'Authorization' header")
+	errUnauthorized             = tberr.New(fiber.ErrUnauthorized.Error())
+	errInternalServerError      = tberr.New(fiber.ErrInternalServerError.Error())
+	errEmptyTxtID               = tberr.New("txt id is empty", "txt id could not be empty")
+	errBadJsonData              = tberr.New("failed to parse request json data")
+)
+
+func sendError(c *fiber.Ctx, err error, status int) error {
+	c.Status(status)
+	return err
+}
